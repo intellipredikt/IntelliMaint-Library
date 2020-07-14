20ab842cffb567ab492e08b8cb3404d70bc2bfb1
Intellipredikt Technologies <55269087+intellipredikt@users.noreply.github.com>
2020-07-14 21:04:19 +0530

    Add files via upload

---
+++ anomaly/group_anomaly/datasets/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "Mohamed-Rafik Bouguelia"
+__license__ = "MIT"
+__email__ = "mohamed-rafik.bouguelia@hh.se"
+
+from .data_loader import load_vehicles
+from .data_loader import load_artificial
+from .data_loader import load_taxi
+from .data_loader import load_vehiclesFE
